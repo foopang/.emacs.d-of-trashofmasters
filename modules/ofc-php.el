e9a95918aa3092130e4f120f930459e2c4457087
Andrea Turso <trashofmasters@gmail.com>
2015-03-01 17:55:07 +0000

    Moved php ctags auto update hook to ofc-php.el.

--- modules/ofc-php.el
+++ modules/ofc-php.el
@@ -7,12 +7,22 @@
   (setq php-lineup-cascaded-calls t)
   (set (make-local-variable 'require-final-newline) t))
 
+;; Activate the `ofc/php-mode-hook' to run my setup my customisations
+;; to php-mode.
 (add-hook 'php-mode-hook 'ofc/php-mode-hook)
 
 ;; Activate `flyspell-prog-mode' minor mode during PHP editing to
 ;; avoid spelling mistakes in comments and strings.
 (add-hook 'php-mode-hook 'flyspell-prog-mode)
 
+;; Activate the `ctags-auto-update-mode' which recreates the TAGS file
+;; with exuberant ctags.
+;;
+;; This function is available since the introduction the
+;; helm-etags-plus package.
+(add-hook 'php-mode-hook 'ctags-auto-update-mode)
+
+;; Use php-mode to visiting .php files.
 (push '("\\.php" . php-mode) auto-mode-alist)
 
 (provide 'ofc-php)
