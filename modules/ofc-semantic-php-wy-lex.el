94574bdd13732b54d276b01f95599fa643bea52a
Andrea Turso <trashofmasters@gmail.com>
2015-03-02 20:20:43 +0000

    Trivial wisent PHP parser implemented and working
    I have finally understood how to enable Semantic
    support for php-mode buffers, how to run the lexer
    to create the AST and parse the tags.
    
    The semantic-grammar-el and semantic-grammar-wy files
    were a source of great information.

---
+++ modules/ofc-semantic-php-wy-lex.el
@@ -0,0 +1,154 @@
+(defconst ofc-semantic-php-number-regexp
+  (eval-when-compile
+    (concat "\\("
+            "\\<[0-9]+[.][0-9]+\\([eE][-+]?[0-9]+\\)?[fFdD]?\\>"
+            "\\|"
+            "\\<[0-9]+[.][eE][-+]?[0-9]+[fFdD]?\\>"
+            "\\|"
+            "\\<[0-9]+[.][fFdD]\\>"
+            "\\|"
+            "\\<[0-9]+[.]"
+            "\\|"
+            "[.][0-9]+\\([eE][-+]?[0-9]+\\)?[fFdD]?\\>"
+            "\\|"
+            "\\<[0-9]+[eE][-+]?[0-9]+[fFdD]?\\>"
+            "\\|"
+            "\\<0[xX][0-9a-fA-F]+[lL]?\\>"
+            "\\|"
+            "\\<[0-9]+[lLfFdD]?\\>"
+            "\\)"
+            ))
+  "Lexer regexp to match Java number terminals.
+Following is the specification of Java number literals.
+
+DECIMAL_LITERAL:
+    [1-9][0-9]*
+  ;
+HEX_LITERAL:
+    0[xX][0-9a-fA-F]+
+  ;
+OCTAL_LITERAL:
+    0[0-7]*
+  ;
+INTEGER_LITERAL:
+    <DECIMAL_LITERAL>[lL]?
+  | <HEX_LITERAL>[lL]?
+  | <OCTAL_LITERAL>[lL]?
+  ;
+EXPONENT:
+    [eE][+-]?[09]+
+  ;
+FLOATING_POINT_LITERAL:
+    [0-9]+[.][0-9]*<EXPONENT>?[fFdD]?
+  | [.][0-9]+<EXPONENT>?[fFdD]?
+  | [0-9]+<EXPONENT>[fFdD]?
+  | [0-9]+<EXPONENT>?[fFdD]
+  ;")
+
+(defconst ofc-semantic-php-label-regex
+  "[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*")
+
+(defvar ofc-semantic-in-php nil
+  "State variable tracking parsing in/out of PHP code.")
+
+(defun ofc-semantic-php--move-to-php-beginning ()
+  (if (re-search-forward "<[%?]" nil t)
+      (cond
+       ((or (looking-at "\\(php\\)?$")
+	    (looking-at "\\(php\\)?[[:space:]])"))
+	(goto-char (match-end 0))
+	'T_NONPHP)
+       ((or (looking-at "=$")
+	    (looking-at "=[[:space:]]"))
+	'T_ECHO_BLOCK)
+       (t
+	(ofc-semantic-php--move-to-php-beginning)))
+    (goto-char (point-max))
+    nil))
+
+(define-lex-regex-analyzer ofc-semantic-php-lex-prologue
+  "Detect and create a prologue token."
+  "<[?%]\\(php\\)?\\([[:space:]]+\\|$\\)"
+  ;; Zing to the end of this brace block.
+  (let ((start (match-beginning 0))
+        (end   (match-end 0)))
+    (semantic-lex-push-token
+     (semantic-lex-token 'PROLOGUE start end))))
+
+(define-lex-regex-analyzer ofc-semantic-php-lex-epilogue
+  "Detect and create an epilogue or percent-percent token."
+  "[%?]>"
+  (let ((start (match-beginning 0))
+        (end   (match-end 0)))
+    (semantic-lex-push-token
+     (semantic-lex-token 'EPILOGUE start end))))
+
+(define-lex-regex-analyzer ofc-semantic-php-lex-heredoc
+  "Detect and create an epilogue or percent-percent token."
+  (concat "<<<[[:blank:]]*\\(" ofc-semantic-php-label-regex "\\)$")
+  (let ((start (match-beginning 0))
+        (end   (progn
+		 (re-search-forward (concat "^" (match-string 1) ";") nil t)
+		 (match-end 0))))
+    (semantic-lex-push-token
+     (semantic-lex-token 'STRING_LITERAL start end))
+    (setq semantic-lex-end-point end)))
+
+(define-lex-analyzer ofc-semantic-php-lex-out-of-php
+  "Detect and create python indentation tokens at beginning of line."
+  (progn
+    (and ofc-semantic-in-php
+	 (looking-at "[[:space:]\n]*[%?]>")
+	 (setq ofc-semantic-in-php nil))
+    (when (not ofc-semantic-in-php)
+      (let ((last-pos (point))
+	    (token (ofc-semantic-php--move-to-php-beginning)))
+	(setq semantic-lex-end-point (point))
+	(when token
+	  (setq ofc-semantic-in-php t)))
+      t)))
+
+;; Define the lexer for this grammar
+(define-lex ofc-semantic-php-lexer
+  "Lexical analyzer that handles php buffers.
+It ignores whitespaces, newlines and comments."
+  ;; Ignore whitespace, newline and comments.
+  semantic-lex-ignore-whitespace
+  semantic-lex-ignore-newline
+
+  ;; Must detect prologue/epilogue before other symbols/keywords.
+  ofc-semantic-php-lex-prologue
+  ofc-semantic-php-lex-epilogue
+
+  ;; Auto-generated analyzers.
+  ofc-semantic-php-wy--<number>-regexp-analyzer
+  ofc-semantic-php-wy--<string>-sexp-analyzer
+
+  ;; Must detect comments after strings because `comment-start-skip'
+  ;; regexp match semicolons inside strings!
+  semantic-lex-ignore-comments
+
+  ;; Must detect keywords before other symbols
+  ofc-semantic-php-wy--<keyword>-keyword-analyzer
+  ofc-semantic-php-wy--<symbol>-regexp-analyzer
+  ofc-semantic-php-wy--<punctuation>-string-analyzer
+  ofc-semantic-php-wy--<block>-block-analyzer
+  semantic-lex-default-action)
+
+;; Taken from Emacs Cedet sources (semantic-grammar.el)
+(defun ofc/semantic-php-lex-buffer ()
+  "Run `ofc-semantic-lex' on current buffer."
+  (interactive)
+  (semantic-lex-init)
+  (setq semantic-lex-analyzer 'ofc-semantic-php-lexer)
+  (let ((token-stream
+         (semantic-lex (point-min) (point-max))))
+    (with-current-buffer (get-buffer-create "*ofc-semantic-php-lex*")
+      (erase-buffer)
+      (pp token-stream (current-buffer))
+      (goto-char (point-min))
+      (pop-to-buffer (current-buffer)))))
+
+(provide 'ofc-semantic-php-wy-lex)
+
+;;; ofc-semantic-wy-lex.el ends here.
--- modules/ofc-semantic-php.el
+++ modules/ofc-semantic-php.el
@@ -1,64 +1,43 @@
 (require 'semantic)
 (require 'semantic/wisent)
 (require 'ofc-semantic-php-wy)
-
-(define-mode-local-override semantic-get-local-variables
-  php-mode ()
-  "Get local values from a specific context.
-Parse the current context for `field_declaration' nonterminals to
-collect tags, such as local variables or prototypes.
-This function override `get-local-variables'."
-  (let ((vars nil)
-        ;; We want nothing to do with funny syntaxing while doing this.
-        (semantic-unmatched-syntax-hook nil))
-    (while (not (semantic-up-context (point) 'function))
-      (save-excursion
-        (forward-char 1)
-        (setq vars
-              (append (semantic-parse-region
-                       (point)
-                       (save-excursion (semantic-end-of-context) (point))
-                       'field_declaration
-                       0 t)
-                      vars))))
-    vars))
+(require 'ofc-semantic-php-wy-lex)
 
 ;;;;
 ;;;; Semantic integration of the Php LALR parser
 ;;;;
-(defun ofc-semantic-php-default-setup ()
+(defun ofc/semantic-php-setup ()
   "Hook run to setup Semantic in `php-mode'.
 Use the alternate LALR(1) parser."
-  (set (make-local-variable 'wisent-in-php) nil)
   (ofc-semantic-php-wy--install-parser)
   (setq
+
    ;; Lexical analysis
-   semantic-lex-number-expression semantic-php-number-regexp
+   semantic-lex-number-expression ofc-semantic-php-number-regexp
    semantic-lex-analyzer 'ofc-semantic-php-lexer
+
    ;; Parsing
    semantic-tag-expand-function 'ofc-semantic-php-expand-tag
+
    ;; Environment
    semantic-imenu-summary-function 'semantic-format-tag-prototype
    imenu-create-index-function 'semantic-create-imenu-index
    semantic-type-relation-separator-character '(".")
    semantic-command-separation-character ";"
    semantic-lex-comment-regex "\\(/\\*\\|//\\|#\\)"
+
    ;; speedbar and imenu buckets name
-   semantic-symbol->name-assoc-list-for-type-parts
-   ;; in type parts
-   '((type     . "Classes")
-     (variable . "Variables")
-     (function . "Methods"))
+   semantic-symbol->name-assoc-list-for-type-parts '((type     . "Classes")
+                                                     (function . "Methods")
+                                                     (variable . "Variables"))
+
    semantic-symbol->name-assoc-list
-   ;; everywhere
-   (append semantic-symbol->name-assoc-list-for-type-parts
-           '((include  . "Includes")
-             (package  . "Package")))
+   (append semantic-symbol->name-assoc-list-for-type-parts '((include  . "Dependencies")
+                                                             (package  . "Namespaces")))
+
    ;; navigation inside 'type children
-   senator-step-at-tag-classes '(function variable)
-   ))
-  ;; Setup phpdoc stuff
-  ;;(semantic-php-doc-setup))
+   senator-step-at-tag-classes '(function variable))
+  (semantic-mode))
 
 (defun ofc-semantic-php-expand-tag (tag)
   "Expand TAG into a list of equivalent tags, or nil.
